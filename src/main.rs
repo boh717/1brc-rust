687cd07bedd7e889bdf41c997078a51d3ab13848
Alberto Coletta <alberto.coletta@commercetools.com>
2024-08-29 19:49:38 +0200

    Add multithreading

--- src/main.rs
+++ src/main.rs
@@ -14,22 +14,66 @@ struct Measurement {
 }
 
 fn main() {
-    let mut accumulator: FxHashMap<&str, Measurement> = FxHashMap::default();
-
     let file = File::open("./measurements.txt").unwrap();
-    let buf = unsafe { Mmap::map(&file).unwrap() };
-    assert!(buf.last() == Some(&b'\n'));
+    let data = unsafe { Mmap::map(&file).unwrap() };
+    let num_threads = std::thread::available_parallelism().unwrap().get();
 
-    let mut start: usize = 0;
-    for index in memchr_iter(b'\n', &buf) {
-        process_line(&buf[start..index], &mut accumulator);
-        start = index + 1;
+    let chunks = get_chunks(&data, num_threads);
+
+    std::thread::scope(|s| {
+        let mut threads_handle = Vec::with_capacity(num_threads);
+        for (start, end) in chunks {
+            let chunk = &data[start..end];
+            let t = s.spawn(|| {
+                let mut chunk_accumulator: FxHashMap<&str, Measurement> = FxHashMap::default();
+                let mut start = 0;
+                for index in memchr_iter(b'\n', chunk) {
+                    process_line(&chunk[start..index], &mut chunk_accumulator);
+                    start = index + 1;
+                }
+                chunk_accumulator
+            });
+            threads_handle.push(t);
+        }
+
+        let mut ordered_accumulator: BTreeMap<&str, Measurement> = BTreeMap::new();
+        for thread in threads_handle {
+            let chunk_accumulator = thread.join().unwrap();
+            for (key, measurement) in chunk_accumulator {
+                ordered_accumulator
+                    .entry(key)
+                    .and_modify(|e| {
+                        e.min = e.min.min(measurement.min);
+                        e.max = e.max.max(measurement.max);
+                        e.sum += measurement.sum;
+                        e.count += measurement.count;
+                    })
+                    .or_insert(measurement);
+            }
+        }
+
+        print_results(&ordered_accumulator);
+    });
+}
+
+fn get_chunks(data: &[u8], num_threads: usize) -> Vec<(usize, usize)> {
+    let chunk_size = data.len() / num_threads;
+    let mut chunks = Vec::with_capacity(num_threads);
+    let mut start = 0;
+    for _ in 0..num_threads - 1 {
+        let end = start + chunk_size;
+        match memchr::memchr(b'\n', &data[end..]) {
+            Some(pos) => {
+                chunks.push((start, end + pos));
+                start = end + pos + 1;
+            }
+            None => {
+                chunks.push((start, data.len()));
+                break;
+            }
+        }
     }
-
-    let mut ordered_accumulator: BTreeMap<&str, Measurement> = BTreeMap::new();
-    ordered_accumulator.extend(accumulator);
-
-    print_results(&ordered_accumulator);
+    chunks
 }
 
 fn process_line<'a>(line: &'a [u8], accumulator: &mut FxHashMap<&'a str, Measurement>) {
